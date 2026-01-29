76e984c8d1349e53db5b463d8341613fd8c95cd8
Matheo Lamiral <matheolamiral@gmail.com>
2026-01-29 16:39:48 -0300

    modificado app principal

--- src/main/java/com/cgelectrosound/gestion/GestionApplication.java
+++ src/main/java/com/cgelectrosound/gestion/GestionApplication.java
@@ -1,25 +1,47 @@
 package com.cgelectrosound.gestion;
 
+import com.cgelectrosound.gestion.utils.NavigationManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GestionApplication extends Application {
+
+	private static ConfigurableApplicationContext springContext;
 
 	public static void main(String[] args) {
 		launch();
 	}
 
 	@Override
+	public void init(){
+		springContext = SpringApplication.run(GestionApplication.class);
+	}
+
+	@Override
 	public void start(Stage stage) throws Exception {
-		var context = SpringApplication.run(GestionApplication.class);
-		var fxml = new FXMLLoader(getClass().getResource("/views/Home.fxml"));
-		var scene = new Scene(fxml.load());
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Home.fxml"));
+
+		loader.setControllerFactory(springContext::getBean);
+
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+
+		NavigationManager.getInstance().setMainScene(scene);
+		NavigationManager.getInstance().setInitialView(root);
+
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	@Override
+	public void stop(){
+		springContext.close();
 	}
 }
